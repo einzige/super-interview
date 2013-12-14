0375b47f7140c5e5ef937f94153255d1575d1d42
Sergei Zinin <zinin@xakep.ru>
2013-12-14 14:27:34 +0800

    Things for fun

--- lib/models/job.rb
+++ lib/models/job.rb
@@ -1,2 +1,3 @@
 class Job < ActiveRecord::Base
+  validates :started_on, presence: true
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe Job do
-  subject { described_class.create(developer: 'John Doe') }
+  subject { described_class.create(developer: 'John Doe', started_on: started_on) }
+  let(:started_on) { Date.today }
+
+  it { should be_valid }
   its(:developer) { should == 'John Doe' }
+
+  context "start date is not set" do
+    let(:started_on) { nil }
+    it { should_not be_valid }
+  end
 end
